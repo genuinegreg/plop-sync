1e6c189faad1c0ba5122b01a84f336f29ddf49f8
Grégoire Audoux <projects@gregoire-audoux.fr>
2013-11-04 16:57:35 +0100

    Update clients and server

--- proxy.js
+++ proxy.js
@@ -4,7 +4,7 @@ var httpProxy = require('http-proxy');
 
 var options = {
   router: {
-    'localhost/api/v1': '127.0.0.1:8080',
+    'localhost/api/v1/': '127.0.0.1:8080/',
     'localhost/': '127.0.0.1:9000/'
   }
 };
